e4f1f77fac0ff97fc4cb88f1dacf28ce073c6c1c
Patrick Alves <cpatrickalves@gmail.com>
2018-09-16 09:29:20 -0300

    Allow custom search string

--- scraping_ebay/spiders/ebay.py
+++ scraping_ebay/spiders/ebay.py
@@ -3,13 +3,30 @@ import scrapy
 
 
 class EbaySpider(scrapy.Spider):
-	name = 'ebay'
-	allowed_domains = ['ebay.com']
-	start_urls = ['https://www.ebay.com/sch/i.html?_from=R40&_sacat=0&_nkw=%22nintendo+switch+console%22&rt=nc&_ipg=200']
+	
+	name = "ebay"
+	allowed_domains = ["ebay.com"]
+	start_urls = ["http://www.ebay.com"]
+
+	# Allow a custom parameter (-a flag in the scrapy command)
+	def __init__(self, search="nintendo switch console"):
+		self.search_string = search
 
 	def parse(self, response):
+		# Extrach the trksid to build a search request	
+		trksid = response.css("input[type='hidden'][name='_trksid']").xpath("@value").extract()[0]       
+		
+		yield scrapy.Request("http://www.ebay.com/sch/i.html?_from=R40&_trksid=" + trksid +
+							 "&_nkw=" + self.search_string.replace(' ','+'), 
+							 callback=self.parse_link)
+
+	# Parse the search results
+	def parse_link(self, response):
+		scrapy.utils.response.open_in_browser(response)
+
 		results = response.xpath('//li[@class="s-item "]')
 
+		# Extract info for each product
 		for product in results:
 			name = product.xpath('.//*[@class="s-item__title"]/text()').extract_first()
 			price = product.xpath('.//*[@class="s-item__price"]/text()').extract_first()
@@ -17,6 +34,7 @@ class EbaySpider(scrapy.Spider):
 			seller_level = product.xpath('.//*[@class="s-item__etrs-text"]/text()').extract_first()
 			location = product.xpath('.//*[@class="s-item__location s-item__itemLocation"]/text()').extract_first()
 
+			# Set default values
 			stars = -1
 			ratings = 0
 
@@ -34,8 +52,7 @@ class EbaySpider(scrapy.Spider):
 			"Stars":stars,
 			"Ratings":ratings
 			}
-
-		#https://www.ebay.com/sch/i.html?_from=R40&_sacat=0&_nkw=%22nintendo+switch+console%22&rt=nc&_pgn=2
+		
 		next_page_url = response.xpath('//*[@class="x-pagination__control"][2]/@href').extract_first()
 		yield scrapy.Request(next_page_url)       	
 
